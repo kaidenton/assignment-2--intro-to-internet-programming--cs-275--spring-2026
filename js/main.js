6cdb02ead1b46bc3d6323f82299d161060491c8d
kaidenton <notnediak@gmail.com>
2026-04-03 17:20:05 -0400

    added all code for it to insert all information onto the webpage and also fixed img path
    only edited data.json because the img path didn't match the img's name

--- js/main.js
+++ js/main.js
@@ -9,6 +9,83 @@
 //
 // DON’T PROCEED UNTIL YOU’RE SURE ESLINT AND EDITORCONFIG ARE WORKING CORRECTLY
 // -----------------------------------------------------------------------------
-window.onload = () => {
-       alert('Test');                    
+
+let currentSlide = 0;
+let slides;
+
+const slidesSections = document.querySelector('.carousel-slides');
+const arrows = document.querySelectorAll('.carousel-navigation a');
+
+const reviews = (data) => {
+       console.log(data);
+       buildCarousel(data.albums);
+       slides = document.querySelectorAll('.slide');
+
+       //Code for Left Arrow
+       document.querySelectorAll('.carousel-navigation a')[0]
+       .addEventListener('click', (e) => {
+              e.preventDefault();
+              if (0 === currentSlide) return;
+              currentSlide--;
+              updateCarousel();
+       })
+       //Code for Right Arrow
+       document.querySelectorAll('.carousel-navigation a')[1]
+       .addEventListener('click', (e) => {
+              e.preventDefault();
+              if (slides.length - 1 === currentSlide) return;
+              currentSlide++;
+              updateCarousel();
+       })
+       document.addEventListener('keydown', (e) => {
+              if (e.key === 'ArrowLeft') {
+                     if (0 === currentSlide) return;
+                     currentSlide--;
+                     updateCarousel();
+              } else if (e.key === 'ArrowRight') {
+                     if (slides.length - 1 === currentSlide) return;
+                     currentSlide++;
+                     updateCarousel();
+              }
+       })
 }
+
+const buildCarousel = (albums) => {
+       albums.forEach((album) => {
+              slidesSections.innerHTML += `
+                     <div class="slide">
+                            <h2>${album.album}</h2>
+                            <h3>
+                                   <a href="${album.url}" target="_blank">
+                                          ${album.artist}
+                                   </a>
+                            </h3>
+                            <img src="${album.cover_image.path}" width="640">
+                            <p class="credit" style="text-align: center;">
+                                   Credit:
+                                   <a href="${album.cover_image.url}" target="_blank">
+                                          ${album.cover_image.credit}
+                                   </a>
+                            </p>
+                            <p>${album.review.content}</p>
+                            <p>
+                                   <strong>
+                                          <a href="${album.review.url}" target="_blank">
+                                                 — ${album.review.source}
+                                          </a>
+                                   </strong>
+                            </p>
+                     </div>
+              `;
+       });
+}
+
+const updateCarousel = () => {
+       const offset = currentSlide * 640;
+       slidesSections.style.transform = `translateX(-${offset}px)`;
+};
+
+const body = document.querySelector('body');
+const script = document.createElement('script');
+script.setAttribute('src', 'json/data.json');
+body.appendChild(script);
